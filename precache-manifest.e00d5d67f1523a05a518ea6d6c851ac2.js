c7c4976aae2f7210ce843fefa2d71595ccf2e638
AnuradhaSD <anuradha.sanjeewa@my.sliit.lk>
2020-04-30 20:54:24 +0530

    Updates

---
+++ precache-manifest.e00d5d67f1523a05a518ea6d6c851ac2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853ee89310e5849fb95c54fa18deac5f",
+    "url": "/reactlogin/index.html"
+  },
+  {
+    "revision": "81c8d40029be98e83049",
+    "url": "/reactlogin/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "ea5c1f8b1c16b5b3ebe7",
+    "url": "/reactlogin/static/css/main.171259aa.chunk.css"
+  },
+  {
+    "revision": "81c8d40029be98e83049",
+    "url": "/reactlogin/static/js/2.c718536a.chunk.js"
+  },
+  {
+    "revision": "0524daf8a2156d89615aff3b1b291e2a",
+    "url": "/reactlogin/static/js/2.c718536a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea5c1f8b1c16b5b3ebe7",
+    "url": "/reactlogin/static/js/main.a96bec57.chunk.js"
+  },
+  {
+    "revision": "f81ebd94c563db586316",
+    "url": "/reactlogin/static/js/runtime-main.d8c45d05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactlogin/static/media/logo.5d5d9eef.svg"
+  }
+]);
